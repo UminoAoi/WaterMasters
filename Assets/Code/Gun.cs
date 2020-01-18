2cca08c1bc8a4681d1e7566cca87cf5270a6a356
UminoAoi <48631554+UminoAoi@users.noreply.github.com>
2020-01-18 16:06:05 +0100

    Player mechanics

--- Assets/Code/Gun.cs
+++ Assets/Code/Gun.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -1,18 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 200;
+    public float jumpSpeed = 200;
+
+    Rigidbody2D rb;
+    bool onGround;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        onGround = false;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            Shoot();
+        }
+
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && onGround)
+        {
+            onGround = false;
+            rb.AddForce(new Vector2(rb.velocity.x, jumpSpeed));
+        }
+        float move = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        rb.velocity = new Vector2(move, rb.velocity.y);
+    }
+
+    void Shoot()
+    {
+
+    }
+
+    void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        onGround = true;
+
+        if (collision.gameObject.layer == 8)
+            Die();
     }
 }
