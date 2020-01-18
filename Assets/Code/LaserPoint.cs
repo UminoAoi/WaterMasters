e011612e741d4a6610ec62712f32056608c8c376
UminoAoi <48631554+UminoAoi@users.noreply.github.com>
2020-01-18 22:59:49 +0100

    Water level created

--- Assets/Code/LaserPoint.cs
+++ Assets/Code/LaserPoint.cs
@@ -21,7 +21,6 @@ public class LaserPoint : MonoBehaviour
 
 private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("COLLISION!");
         laser.StartShink();
         Destroy(gameObject);
     }
