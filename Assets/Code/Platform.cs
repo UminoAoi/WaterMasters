b75dd80cc270a0ec30cd7d4af7bb9110e29c18d5
Joanna <32140567+joannasoltysiak@users.noreply.github.com>
2020-01-18 16:06:19 +0100

    other

--- Assets/Code/Platform.cs
+++ Assets/Code/Platform.cs
@@ -2,24 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rain : MonoBehaviour
+public class Platform : MonoBehaviour
 {
-    public WaterState state;
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    void ChangeState()
-    {
-
+        
     }
 }
