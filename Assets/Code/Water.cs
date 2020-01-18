509e01eb3678d446e71c1fce6eb5f4aaae04a39d
Joanna <32140567+joannasoltysiak@users.noreply.github.com>
2020-01-18 15:31:01 +0100

    Water(State)
    Making a WaterState.

---
+++ Assets/Code/Water.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    
+}
+
+public enum WaterState
+{
+    Frozen,
+    Liquid,
+    Steam
+}
