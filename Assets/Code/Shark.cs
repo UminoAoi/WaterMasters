76eb5a41ecfa07e9cf1b2c871dab1b5cc2f9b5fd
UminoAoi <48631554+UminoAoi@users.noreply.github.com>
2020-01-19 05:31:27 +0100

    Shark and rock works

--- Assets/Code/Shark.cs
+++ Assets/Code/Shark.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Shark : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform player;
+    public float speed = 1;
+
+    public Animator animator;
+    bool isRight = true;
+
+
+    private void Update()
     {
-        
+        Vector2 target = new Vector2(player.position.x, transform.position.y);
+        if(target.x < transform.position.x && isRight)
+        {
+            isRight = false;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }else if(target.x > transform.position.x && !isRight)
+        {
+            isRight = true;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        GameObject obj = collision.gameObject;
+
+        if (obj.layer == 17)
+        {
+            Bite();
+            Destroy(obj);
+        }else if (obj.layer == 19 && obj.transform.position.y > transform.position.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Bite()
+    {
+        /////
     }
 }
