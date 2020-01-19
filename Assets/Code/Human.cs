5b7c925164d201977d7113bd3e7ecff50cf6b0b5
UminoAoi <48631554+UminoAoi@users.noreply.github.com>
2020-01-19 02:34:32 +0100

    WaterLevel works properly

---
+++ Assets/Code/Human.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    HumanState state = HumanState.Ice;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum HumanState
+{
+    Ice,
+    Normal
+}
