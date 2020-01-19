a4a8f65df158994b506d0ebc70295176ce615e28
UminoAoi <48631554+UminoAoi@users.noreply.github.com>
2020-01-19 07:00:35 +0100

    Dialogue system

---
+++ Assets/Code/DialogueMaker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueMaker : MonoBehaviour
+{
+    Dialogue dialogue;
+    public string text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue = FindObjectOfType<Dialogue>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        dialogue.ShowNextDialogue(text);
+    }
+}
