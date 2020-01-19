5f02503ad0531333c3a2eabc441df6ce3d0b0a88
UminoAoi <48631554+UminoAoi@users.noreply.github.com>
2020-01-19 04:01:58 +0100

    Human prefab works properly

--- Assets/Code/Human.cs
+++ Assets/Code/Human.cs
@@ -6,10 +6,31 @@ public class Human : MonoBehaviour
 {
     HumanState state = HumanState.Ice;
 
-    // Update is called once per frame
-    void Update()
+    public Animator animator;
+
+    public ParticleSystem steam;
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        GameObject obj = collision.gameObject;
+        if (obj.layer == 9 && obj.CompareTag("Steam"))
+        {
+            UnFroze();
+        }
+    }
+
+    void UnFroze()
+    {
+        animator.SetTrigger("unfroze");
+        StartCoroutine(Steam());
+        state = HumanState.Normal;
+    }
+
+    IEnumerator Steam()
+    {
+        steam.Play();
+        yield return new WaitForSeconds(1);
+        steam.Stop();
     }
 }
 
